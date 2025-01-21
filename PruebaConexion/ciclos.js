cef57016a29c26e62bce44de3629628e804ebf87
dam2 - Tubío Ferreiro <A23HugoTF@sanclemente.local>
2025-01-21 09:24:28 +0100

    cambie la prueba de select de manera que funcione con nuestra base de datos

--- PruebaConexion/ciclos.js
+++ PruebaConexion/ciclos.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
-const dbPath = '..\\BD\\horariappBD';
+// Esto por algún motivo no me funciona sin ruta absoluta
+const dbPath = 'C:\\Users\\a23hugotf\\Desktop\\nuevacarpeta\\BD\\horariAppBD';
 const cors = require('cors'); 
 
 app.use(cors());
-app.get('/pacientes', async (req, res) => {
+// app.get busca en "http://localhost:3000" que es el fichero de la base de datos una tabla "ciclos"
+app.get('/ciclos', async (req, res) => {
     const db = new sqlite3.Database(dbPath);
     try {
         const rows = await new Promise((resolve, reject) => {
-            db.all('SELECT * FROM Paciente', [], (err, rows) => {
+            db.all('SELECT * FROM Ciclos', [], (err, rows) => {
                 if (err) reject(err);
                 else resolve(rows);
             });
@@ -23,3 +25,5 @@ app.get('/pacientes', async (req, res) => {
 });
 
 app.listen(3000, () => console.log('Servidor corriendo en http://localhost:3000'));
+
+// El servidor se lanza con el comando: " node .\PruebaConexion\ciclos.js "
