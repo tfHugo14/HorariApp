0f2bc4e388f65cc5621b7697d85558ada257e45a
dam2 - Recouso Varela <A22DanielRV@sanclemente.local>
2025-02-07 12:11:53 +0100

    comentado path en server general

--- ServerYScripts/server.js
+++ ServerYScripts/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // ****************** MODULOS Y PROFESORES (DANI) ******************
 
