23f84d1e9af958e91ce0ef01f5872b3c5ca15807
dam2 - Tubío Ferreiro <A23HugoTF@sanclemente.local>
2025-02-12 09:36:56 +0100

    cambios en las plantillas

---
+++ plantillaMovil/administrarAlumno.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const botones = document.querySelectorAll(".boton");
+
+    botones.forEach(boton => {
+        boton.addEventListener("click", function () {
+            const itemHeader = this.closest(".itemHeader"); 
+            const itemBody = itemHeader.nextElementSibling;
+            const flecha = this.querySelector(".flecha");
+
+            if (itemBody.classList.contains("mostrar")) {
+                itemBody.style.maxHeight = itemBody.scrollHeight + "%"; // Fijar altura antes de contraer
+                setTimeout(() => {
+                    itemBody.classList.remove("mostrar");
+                    itemHeader.classList.remove("abierto");
+                    flecha.classList.remove("rotar");
+                }, 10); 
+            } else {
+                itemBody.classList.add("mostrar");
+                itemHeader.classList.add("abierto");
+                flecha.classList.add("rotar");
+                itemBody.style.maxHeight = itemBody.scrollHeight + "%"; // Expandir suavemente
+            }
+        });
+    });
+});
