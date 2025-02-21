ed7bb9b3a086bb9f475c801beec7d9f2a403d3c1
dam2 - Tubío Ferreiro <A23HugoTF@sanclemente.local>
2025-02-21 12:33:56 +0100

    remodelacion de la BD

--- BD/SQLHorarios.sql
+++ BD/SQLHorarios.sql
@@ -1,72 +1,66 @@
 CREATE TABLE Ciclos (
-    id_ciclos TEXT PRIMARY KEY,
+    id_ciclos INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre TEXT NOT NULL,
     descripcion TEXT NOT NULL,
     duracion INTEGER NOT NULL
 );
 
 CREATE TABLE Profesor (
-    id_profesor TEXT PRIMARY KEY,
+    id_profesor INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre TEXT
 );
 
 CREATE TABLE Modulos (
-    id_modulo TEXT PRIMARY KEY,
-    duracion INTEGER,
+    id_modulo INTEGER PRIMARY KEY AUTOINCREMENT,
+    curso INTEGER,
     nombre TEXT,
+    siglas TEXT,
     horas_semanales INTEGER,
     descripcion TEXT,
-    id_ciclos TEXT,
-    id_profesor TEXT,
+    id_ciclos INTEGER,
+    id_profesor INTEGER,
     FOREIGN KEY (id_ciclos) REFERENCES Ciclos (id_ciclos),
     FOREIGN KEY (id_profesor) REFERENCES Profesor (id_profesor)
 );
 
 CREATE TABLE Sesiones (
-    id_sesiones TEXT PRIMARY KEY,
+    id_sesiones INTEGER PRIMARY KEY AUTOINCREMENT,
     hora_ini TEXT,
     hora_fin TEXT,
     dia TEXT,
     aula INTEGER,
     descripcion TEXT,
     id_modulos INTEGER,
-    FOREIGN KEY (id_modulos) REFERENCES Modulos
+    FOREIGN KEY (id_modulos) REFERENCES Modulos (id_modulo)
 );
 
 CREATE TABLE Estudiante (
-    id_usuario TEXT PRIMARY KEY,
+    id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre TEXT,
+    nombreCompleto TEXT,
     contrasenha TEXT,
     dni TEXT
 );
 
 CREATE TABLE Administrador (
-    id_usuario TEXT PRIMARY KEY,
+    id_admin INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre TEXT,
     contrasenha TEXT,
     dni TEXT
 );
 
 CREATE TABLE matricular_ciclo (
-    id_ciclos TEXT,
-    id_usuario TEXT,
+    id_ciclos INTEGER,
+    id_usuario INTEGER,
     PRIMARY KEY (id_ciclos, id_usuario),
     FOREIGN KEY (id_ciclos) REFERENCES Ciclos (id_ciclos),
     FOREIGN KEY (id_usuario) REFERENCES Estudiante (id_usuario)
 );
 
 CREATE TABLE matricular_modulos (
-    id_modulos TEXT,
-    id_usuario TEXT,
+    id_modulos INTEGER,
+    id_usuario INTEGER,
     PRIMARY KEY (id_modulos, id_usuario),
     FOREIGN KEY (id_modulos) REFERENCES Modulos (id_modulo),
     FOREIGN KEY (id_usuario) REFERENCES Estudiante (id_usuario)
-);
-
-CREATE TABLE administrar (
-    id_ciclos TEXT,
-    id_usuario TEXT,
-    PRIMARY KEY (id_ciclos, id_usuario),
-    FOREIGN KEY (id_ciclos) REFERENCES Ciclos (id_ciclos),
-    FOREIGN KEY (id_usuario) REFERENCES Administrador (id_usuario)
 );
